f88bf1055637ebce5538dc3343bff73e29f874eb
Osborn Zhang <sstteeaammuser101@gmail.com>
2025-02-13 00:46:52 +0000

    commit

--- robot/drive/Drive.java
+++ robot/drive/Drive.java
@@ -1,5 +1,17 @@
 package robot.drive;
 
+private final DifferentialDriveOdometry odometry;
+
+    odometry = new DifferentialDriveOdometry(
+            new Rotation2d(), 
+            0, 
+            0, 
+            new Pose2d());
+    
+    private void updateOdometry(Rotation2d rotation) {
+    odometry.update(rotation, leftEncoder.getPosition(), rightEncoder.getPosition());
+  }
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -11,10 +23,18 @@ import java.awt.List;
 import com.revrobotics.CANSparkBase.IdleMode;
 
 public class Drive extends SubsystemBase {
+
+    gyro.reset();
+
     private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
     private final CANSparkMax leftFollower = new CANSparkMax(Ports.Drive.LEFT_FOLLOWER, MotorType.kBrushless);
     private final CANSparkMax rightLeader = new CANSparkMax(Ports.Drive.RIGHT_LEADER_LEADER, MotorType.kBrushless);
     private final CANSparkMax rightFollower = new CANSparkMax(Ports.Drive.RIGHT_FOLLOWER, MotorType.kBrushless);
+
+    private final RelativeEncoder leftEncoder = leftLeader.getEncoder();
+    private final RelativeEncoder rightEncoder = rightLeader.getEncoder();
+
+    private final AnalogGyro gyro = new AnalogGyro(Ports.Drive.GYRO_CHANNEL);
 
     public Drive() {
         for (CANSparkMax spark : List.of(leftLeader, leftFollower, rightLeader, rightFollower)) {
@@ -30,5 +50,21 @@ public class Drive extends SubsystemBase {
     private void drive(double leftSpeed, double rightSpeed) {
         leftLeader.set(leftSpeed);
         rightLeader.set(rightSpeed);
+    }
+
+    public Pose2d pose() {
+        return odometry.getPoseMeters();
+    }
+
+    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(FF.kS, FF.kV);
+
+    private final PIDController leftPIDController =
+      new PIDController(PID.kP, PID.kI, PID.kD);
+    private final PIDController rightPIDController =
+      new PIDController(PID.kP, PID.kI, PID.kD);
+
+    @Override 
+    public void periodic() {
+        updateOdometry(gyro.getRotation2d());
     }
 }
